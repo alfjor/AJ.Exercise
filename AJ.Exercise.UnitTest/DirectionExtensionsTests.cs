b83205d7bde70c0b600882ef0d6d163ce4b14339
alfjor <alfred.jorgensson@gmail.com>
2020-06-04 07:06:25 +0200

    added some unittests

---
+++ AJ.Exercise.UnitTest/DirectionExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using AJ.Exercise.Enums;
+using AJ.Exercise.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJ.Exercise.UnitTest
+{
+
+    [TestClass]
+    public class DirectionExtensionsTests
+    {
+        [TestMethod]
+        public void When_rotate_clockwise_from_north_it_should_return_east()
+        {
+            // Arrange
+
+            var value = Direction.North;
+
+            // Act
+
+            var result = value.RotateClockwise();
+
+            // Assert
+
+            Assert.AreEqual(Direction.East, result);
+        }
+
+        [TestMethod]
+        public void When_rotate_counterclockwise_from_west_it_should_return_south()
+        {
+            // Arrange
+
+            var value = Direction.West;
+
+            // Act
+
+            var result = value.RotateCounterClockwise();
+
+            // Assert
+
+            Assert.AreEqual(Direction.South, result);
+        }
+    }
+}
---
+++ AJ.Exercise.UnitTest/StringExtenstionsTests.cs
@@ -0,0 +1,29 @@
+﻿using AJ.Exercise.Enums;
+using AJ.Exercise.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJ.Exercise.UnitTest
+{
+    [TestClass]
+    public class StringExtenstionsTests
+    {
+        [TestMethod]
+        public void When_mapping_1_to_command_it_should_return_forward()
+        {
+            // Arrange
+
+            var value = "1";
+
+            // Act
+
+            var result = value.MapStringToCommand();
+
+            // Assert
+
+            Assert.AreEqual(Command.Forward, result);
+        }
+    }
+}
